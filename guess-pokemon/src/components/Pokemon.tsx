a3bb3370c4f1934f25bda1c2fb44c4dcf1eb9420
jnvillalba <joaquinvillalba8@gmail.com>
2023-02-27 16:43:53 -0300

    first commit

---
+++ guess-pokemon/src/components/Pokemon.tsx
@@ -0,0 +1,179 @@
+import { FormEvent, useState } from "react";
+
+const Pokelist = [
+  "bulbasaur",
+  "ivysaur",
+  "venusaur",
+  "charmander",
+  "charmeleon",
+  "charizard",
+  "squirtle",
+  "wartortle",
+  "blastoise",
+  "caterpie",
+  "metapod",
+  "butterfree",
+  "weedle",
+  "kakuna",
+  "beedrill",
+  "pidgey",
+  "pidgeotto",
+  "pidgeot",
+  "rattata",
+  "raticate",
+  "spearow",
+  "fearow",
+  "ekans",
+  "arbok",
+  "pikachu",
+  "raichu",
+  "sandshrew",
+  "sandslash",
+  "nidoran",
+  "nidorina",
+  "nidoqueen",
+  "nidorino",
+  "nidoking",
+  "clefairy",
+  "clefable",
+  "vulpix",
+  "ninetales",
+  "jigglypuff",
+  "wigglytuff",
+  "zubat",
+  "golbat",
+  "oddish",
+  "gloom",
+  "vileplume",
+  "paras",
+  "parasect",
+  "venonat",
+  "venomoth",
+  "diglett",
+  "dugtrio",
+  "meowth",
+  "persian",
+  "psyduck",
+  "golduck",
+  "mankey",
+  "primeape",
+  "growlithe",
+  "arcanine",
+  "poliwag",
+  "poliwhirl",
+  "poliwrath",
+  "abra",
+  "kadabra",
+  "alakazam",
+  "machop",
+  "machoke",
+  "machamp",
+  "bellsprout",
+  "weepinbell",
+  "victreebel",
+  "tentacool",
+  "tentacruel",
+  "geodude",
+  "graveler",
+  "golem",
+  "ponyta",
+  "rapidash",
+  "slowpoke",
+  "slowbro",
+  "magnemite",
+  "magneton",
+  "farfetchd",
+  "doduo",
+  "dodrio",
+  "seel",
+  "dewgong",
+  "grimer",
+  "muk",
+  "shellder",
+  "cloyster",
+  "gastly",
+  "haunter",
+  "gengar",
+  "onix",
+  "drowzee",
+  "hypno",
+  "krabby",
+  "kingler",
+  "voltorb",
+  "electrode",
+  "exeggcute",
+  "exeggutor",
+  "cubone",
+  "marowak",
+  "hitmonlee",
+  "hitmonchan",
+  "lickitung",
+  "koffing",
+  "weezing",
+  "rhyhorn",
+  "rhydon",
+  "chansey",
+  "tangela",
+  "kangaskhan",
+  "horsea",
+  "seadra",
+  "goldeen",
+  "seaking",
+  "staryu",
+  "starmie",
+  "mr mime",
+  "scyther",
+  "jynx",
+  "electabuzz",
+  "magmar",
+  "pinsir",
+  "tauros",
+  "magikarp",
+  "gyarados",
+  "lapras",
+  "ditto",
+  "eevee",
+];
+
+const MATCH = Math.floor(Math.random() * Pokelist.length);
+
+type Form = HTMLFormElement & {
+  pokemon: HTMLInputElement;
+};
+
+export default function Pokemon() {
+  const [hasWon, toggleWon] = useState(false);
+  function handleSubmit(event: FormEvent<Form>) {
+    event.preventDefault();
+    const { pokemon } = event.currentTarget;
+
+    if (pokemon.value.toLowerCase() === Pokelist[MATCH]) {
+      toggleWon(true);
+      alert("You won!");
+    }
+  }
+  return (
+    <div>
+      <img
+        height={412}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          MATCH + 1
+        }.png`}
+        style={{
+          imageRendering: "pixelated",
+          filter: hasWon ? "" : "brightness(0) invert(1)",
+        }}
+        width={412}
+      />
+      {hasWon ? (
+        <button onClick={location.reload}> Play Again</button>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="pokemon"></input>
+          <button type="submit"></button>
+        </form>
+      )}
+    </div>
+  );
+}
+// crear lista de nombres de Pokemon
